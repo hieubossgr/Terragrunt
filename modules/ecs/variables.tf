114c82995944f1f2e32792cbfb8a168b6703da1b
hieu_nt22 <hieunt@nami.trade>
2024-09-12 06:40:55 +0700

    update ecs

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,20 +1,20 @@
 variable "cluster_ecs_name" {
-  description = "Tên của ECS cluster"
+  description = "Name of ecs cluster"
   type        = string
 }
 
 variable "task_family" {
-  description = "Tên family của ECS task definition"
+  description = "Name of family ECS task definition"
   type        = string
 }
 
 variable "task_cpu" {
-  description = "Số lượng CPU cho task"
+  description = "CPU number for task"
   type        = string
 }
 
 variable "task_memory" {
-  description = "Dung lượng bộ nhớ cho task"
+  description = "RAM number for task"
   type        = string
 }
 
@@ -24,56 +24,56 @@ variable "container_name" {
 }
 
 variable "container_image" {
-  description = "Ảnh Docker cho container"
+  description = "Image Docker for container"
   type        = string
 }
 
 variable "container_cpu" {
-  description = "Số lượng CPU cho container"
+  description = "CPU number for container"
   type        = number
 }
 
 variable "container_memory" {
-  description = "Dung lượng bộ nhớ cho container"
+  description = "RAM number for container"
   type        = number
 }
 
 variable "container_port" {
-  description = "Cổng container sẽ lắng nghe"
+  description = "Container port"
   type        = number
 }
 
 variable "service_name" {
-  description = "Tên của ECS service"
+  description = "Name of ECS service"
   type        = string
 }
 
 variable "desired_count" {
-  description = "Số lượng task mong muốn"
+  description = "Number of task"
   type        = number
 }
 
 variable "private_subnets" {
-  description = "Danh sách các subnet private"
+  description = "subnets private"
   type        = list(string)
 }
 
 variable "security_group_id" {
-  description = "ID của security group"
+  description = "Security group id"
   type        = string
 }
 
 variable "target_group_arn" {
-  description = "ARN của target group ALB"
+  description = "ARN of target group ALB"
   type        = string
 }
 
 variable "max_capacity" {
-  description = "Số lượng tối đa task cho auto scaling"
+  description = "Max capacity task for auto scaling"
   type        = number
 }
 
 variable "min_capacity" {
-  description = "Số lượng tối thiểu task cho auto scaling"
+  description = "Min capacity task for auto scaling"
   type        = number
 }
